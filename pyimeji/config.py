5a07ebeb39f1b1bf322e5ffcc7133300193d04bd
xrotwang <xrotwang@googlemail.com>
2014-11-10 17:16:33 +0100

    added tests for config

--- pyimeji/config.py
+++ pyimeji/config.py
@@ -9,15 +9,16 @@ APP_DIRS = AppDirs('pyimeji')
 
 class Config(RawConfigParser):
     def __init__(self, **kw):
+        config_dir = kw.pop('config_dir', None) or APP_DIRS.user_config_dir
         RawConfigParser.__init__(self, kw)
 
-        cfg_path = os.path.join(APP_DIRS.user_config_dir, 'config.ini')
+        cfg_path = os.path.join(config_dir, 'config.ini')
         if os.path.exists(cfg_path):
-            assert os.path.is_file(cfg_path)
+            assert os.path.isfile(cfg_path)
             self.read(cfg_path)
         else:
-            if not os.path.exists(APP_DIRS.user_config_dir):
-                os.mkdir(APP_DIRS.user_config_dir)
+            if not os.path.exists(config_dir):
+                os.mkdir(config_dir)
             with open(cfg_path, 'w') as fp:
                 self.write(fp)
 
---
+++ pyimeji/tests/test_config.py
@@ -0,0 +1,36 @@
+from __future__ import unicode_literals
+import os
+from tempfile import mkdtemp
+from unittest import TestCase
+from shutil import rmtree
+from io import open
+
+
+class ConfigTest(TestCase):
+    def setUp(self):
+        self.tmp = mkdtemp()
+        self.cfg = os.path.join(self.tmp, '.config')
+
+    def tearDown(self):
+        rmtree(self.tmp, ignore_errors=True)
+
+    def test_new_config(self):
+        from pyimeji.config import Config
+
+        self.assertFalse(os.path.exists(self.cfg))
+        cfg = Config(config_dir=self.cfg)
+        self.assertTrue(os.path.exists(self.cfg))
+
+    def test_existing_config(self):
+        from pyimeji.config import Config
+
+        os.mkdir(self.cfg)
+        with open(os.path.join(self.cfg, 'config.ini'), 'w', encoding='utf8') as fp:
+            fp.write("""\
+[section]
+option = 12
+""")
+
+        cfg = Config(config_dir=self.cfg)
+        self.assertEqual(cfg.get('section', 'option'), '12')
+
