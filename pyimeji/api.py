1edb6320765b2e9729bc81bcaa46ab28507652d7
ioverka <overkamp@mpdl.mpg.de>
2016-06-16 22:46:47 +0200

    minor revison of ImejiError message

--- pyimeji/api.py
+++ pyimeji/api.py
@@ -154,10 +154,12 @@ class Imeji(object):
                     log.error(err_message)
                     if hasattr(res, 'text'):
                         log.error(res.text[:1000])
-                        res_json = res.json()
-                        if "error" in res_json and "exceptionReport" in res_json["error"] and "title" in res_json["error"]:
-                            err_message += "\nDetails from response: " + res_json["error"]["title"] + ". " + res_json["error"]["exceptionReport"]
-                    else:
+                        try:
+                            res_json = res.json()
+                            if "error" in res_json and "exceptionReport" in res_json["error"] and "title" in res_json["error"]:
+                                err_message += "\nDetails from response: " + res_json["error"]["title"] + ". " + res_json["error"]["exceptionReport"]
+                        except:
+                            print "hasi"
                         log.error(res)
                     raise ImejiError(err_message, res)                    
 
